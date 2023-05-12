5c8847f5b51d8df1c0016c77008d6cf8a04cd40b
倪传方 <1290274972@qq.com>
2023-05-13 05:59:00 +0800

    ✨ feat: 自动生成博客封面

--- handle_img.py
+++ handle_img.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python
 # coding=utf-8
+from requests import Response
 from PIL import Image
 import requests
 import re
 import base64
 import os
-
+from io import BytesIO
 # 自动生成文档的图片 并将该图片转为webp格式
 
 pattern = re.compile('.(png|jpeg|jpg)$')
@@ -26,6 +27,20 @@ def file_to_webp(input_path:str,output_path:str):
         im.thumbnail((1200,900), Image.ANTIALIAS) #重新设置图片大小
         im.save(output_path) #保存
 
+def url_to_webp(img_url:str,img_path:str):
+    """url转webp
+
+    Args:
+        img_url (str): 图片url
+        img_path (str): 保存的图片路径 
+    """
+    res:Response = requests.get(img_url)
+    base64_data = base64.b64encode(res.content).decode()
+    imgdata = base64.b64decode(base64_data)
+    im = Image.open(BytesIO(imgdata))
+    im.thumbnail((1200,900), Image.ANTIALIAS) #重新设置图片大小
+    im.save(img_path)
+
 # 将目录的图片转webp
 def dir_to_webp(input_dir:str):
     for dir_path,dir_list,file_list in os.walk(input_dir):
@@ -35,13 +50,73 @@ def dir_to_webp(input_dir:str):
             file_to_webp(dir_path.replace('\\','/')+'/'+file,dir_path.replace('\\','/'))
             os.remove(dir_path+'/'+file)
 
-if __name__ == '__main__':
-    dir_to_webp(f'./source')
+def random_img(file_path:str):
+    """随机一张图片
+
+    Args:
+        path (str): 保存的文件路径
+    """    
     base64_data = requests.api.get('https://crawler.vencenter.cn/wallpaper/random')
     #base64_data转换成图片
-    imgdata = base64.b64decode(base64_data)
-    with open('test.webp','wb') as file:
-        file.write(imgdata)
+    imgdata = base64.b64decode(base64_data.text)
+    im = Image.open(BytesIO(imgdata))
+    im.thumbnail((1200,900), Image.ANTIALIAS) #重新设置图片大小
+    im.save(f'./source{file_path}')
+
+if __name__ == '__main__':
+    for dir_path,dir_list,file_list in os.walk('./source/_posts'):
+        for file in file_list:
+            md_name = file.split('.')[0]
+            new_md = ''
+            # 标记header的数量
+            count = 0
+            index = 0
+            need_rewrite:bool = False
+            rewrite_lines:list[str] = []
+            is_update = False
+            with open(dir_path+'/'+file,'r',encoding='utf-8') as md_file:
+                # 若干操作 修改html内容
+                lines = md_file.readlines()
+                for line in lines:
+                    if count == 2:
+                        # 直接输出index之后的内容
+                        if not is_update:
+                            rewrite_lines = rewrite_lines+lines[len(rewrite_lines)-1:]
+                        else:
+                            rewrite_lines = rewrite_lines+lines[len(rewrite_lines):]
+                        break
+                    if line.rfind('cover') == 0 :
+                        handled_line = line.strip().replace(' ', '').replace('\n','')
+                        if len(handled_line) > 6 and handled_line.endswith(('jpg','jpeg','png','webp')):
+                            # 已有封面 跳过
+                            break
+                        else:
+                            # 插入封面
+                            new_cover = f'/img/{md_name}/{md_name}.webp'
+                            random_img(new_cover)
+                            line = f'cover: {new_cover}\n'
+                            is_update = True
+                            # 需要重写
+                            need_rewrite = True    
+                    if line.startswith('---'):
+                        if count == 1:
+                            # 插入封面
+                            if not need_rewrite:
+                                new_cover = f'/img/{md_name}/{md_name}.webp'
+                                random_img(new_cover)
+                                rewrite_lines.append(f'cover: {new_cover}\n')
+                                index = index + 1
+                            need_rewrite = True
+                        count = count + 1
+                    index = index + 1
+                    rewrite_lines.append(line)
+            # 保存到新的md文件
+            if need_rewrite:
+                # 只重写没有封面的文档
+                with open(f'./source/_posts/{file}','w+',encoding='utf-8') as md:
+                    md.writelines(rewrite_lines)
+    
+    # url_to_webp('https://w.wallhaven.cc/full/yx/wallhaven-yxqzpd.jpg','result.webp')
 
 
 
---
+++ source/_posts/test.md
@@ -0,0 +1,10 @@
+---
+title: test
+feature: false
+date: 2023-05-13 05:56:50
+tags: test
+categories: 测试
+cover: 
+---
+
+## 测试封面生成
