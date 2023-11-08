7394a721fb8603099ba89c8744be3ce700a63da6
nichuanfang <f18326186224@gmail.com>
2023-11-08 20:26:47 +0800

    ✨ feat:测试部署

--- transform.py
+++ transform.py
@@ -1,4 +1,8 @@
 import os
+import datetime
+
+# 获取现在的日期 格式yyyy-MM-dd HH:mm:ss 时区为中国
+date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 fluid_posts_path = os.path.join(os.path.pardir,'hexo-blog-fluid','source','_posts')
 fluid_img_path = os.path.join(os.path.pardir,'hexo-blog-fluid','source','img','post')
@@ -16,6 +20,28 @@ for root, dirs, files in os.walk('posts'):
                     os.rename(os.path.join(post_root,post_file),os.path.join(fluid_img_path,dir,post_file))
                 # 文档
                 elif post_file == 'index.md':
+                    # 处理post_file的头部
+                    with open(os.path.join(post_root,post_file),'r',encoding='utf-8') as f:
+                        lines = f.readlines()
+                        first_index = lines.index('---\n')
+                        last_index = lines.index('---\n',first_index+1)
+                        head_lines = lines[first_index+1:last_index]
+                        left_lines = lines[last_index+1:]
+                        
+                        # 对head_lines进行处理
+                        head_lines.append(f'title: {dir}\n')
+                        head_lines.append(f'date: {date}\n')
+                        # 如果当前目录下有banner.jpg则设置banner_img
+                        if 'banner.jpg' in post_files:
+                            head_lines.append(f'banner_img: /img/post/{dir}/banner.jpg\n')
+                        # 如果当前目录下有index.jpg则设置index_img
+                        if 'index.jpg' in post_files:
+                            head_lines.append(f'index_img: /img/post/{dir}/index.jpg\n')
+                    
+                    head_lines.insert(0,'---\n')
+                    head_lines.append('---\n')
+                    new_lines = head_lines + left_lines
+                    
                     # 如果_posts文件夹不存在则创建
                     if not os.path.exists(fluid_posts_path):
                         os.mkdir(fluid_posts_path)
