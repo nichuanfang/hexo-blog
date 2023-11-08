ce652b5d5ce54f901535f748b011884e5504d268
nichuanfang <f18326186224@gmail.com>
2023-11-08 17:27:01 +0800

    1✨ feat: release

--- scripts/bgimage.js
+++ scripts/bgimage.js
@@ -1,6 +1,6 @@
 hexo.extend.filter.register('theme_inject', function (injects) {
-  injects.bodyEnd.raw(
+  injects.bodyBegin.raw(
     'menhera',
-    '<div id="background-image" style="display: block; left: 330.067px;"> <img src="/img/bg/bg01.png" alt="Background image"> </div>'
+    '<div id="background-image" style="display: none; left: 330.067px;"> <img src="/img/bg/bg01.png" alt="Background image"> </div>'
   )
 })
