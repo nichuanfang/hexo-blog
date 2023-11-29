6f701324eafced87a477f50d8dc17620fbcadacf
f18326186224 <f18326186224@gmail.com>
2023-11-29 16:14:55 +0800

    pref: test

--- source/js/subtitle.js
+++ source/js/subtitle.js
@@ -1,6 +1,11 @@
 fetch('https://v2.jinrishici.com/one.json', {
   method: 'GET',
   mode: 'cors', // 设置跨域请求模式
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'X-User-Token': 'xNJL0fQDhPz3++IrWZte3QnBlkeSC+J/',
+  },
 })
   .then((response) => response.json())
   .then((data) => {
