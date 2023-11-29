63160d6262b840be2033e9228936d2bf04c1f12a
f18326186224 <f18326186224@gmail.com>
2023-11-29 16:42:36 +0800

    pref: 更换请求接口

--- source/js/subtitle.js
+++ source/js/subtitle.js
@@ -13,20 +13,20 @@
 // }
 // xhr.send()
 
-fetch('https://v2.jinrishici.com/sentence', {
-  method: 'GET',
-  mode: 'cors', // 设置跨域请求模式
-  headers: {
-    'X-User-Token': 'xNJL0fQDhPz3++IrWZte3QnBlkeSC+J/',
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    // 处理返回的数据
-    console.log(data)
-    console.log(data.data.content) // 获取每日一句内容
-  })
-  .catch((error) => {
-    // 处理错误
-    console.error('Error:', error)
-  })
+// fetch('https://v2.jinrishici.com/sentence', {
+//   method: 'GET',
+//   mode: 'cors', // 设置跨域请求模式
+//   headers: {
+//     'X-User-Token': 'xNJL0fQDhPz3++IrWZte3QnBlkeSC+J/',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // 处理返回的数据
+//     console.log(data)
+//     console.log(data.data.content) // 获取每日一句内容
+//   })
+//   .catch((error) => {
+//     // 处理错误
+//     console.error('Error:', error)
+//   })
