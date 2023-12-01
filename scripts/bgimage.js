d386381647ed49445ed85cc3177faa67c833c0c5
f18326186224 <f18326186224@gmail.com>
2023-12-01 20:23:49 +0800

    feat: 开启懒加载 修复刷新空白

--- scripts/bgimage.js
+++ scripts/bgimage.js
@@ -3,4 +3,9 @@ hexo.extend.filter.register('theme_inject', function (injects) {
     'menhera',
     '<div id="background-image" style="display: none; left: 330.067px;"> <img src="/img/bg/bg01.png" alt="Background image"> </div>'
   )
+  // 修复刷新空白
+  injects.head.raw(
+    'default',
+    '<style> #live2d-widget { display: none; } </style>'
+  )
 })
