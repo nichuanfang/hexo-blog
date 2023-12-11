cc4997bc32d45e95769358f55474873d80fed4c1
f18326186224 <f18326186224@gmail.com>
2023-12-12 03:03:57 +0800

    feat: 电影实时数据展示

--- source/js/custom.js
+++ source/js/custom.js
@@ -1,3 +1,65 @@
+// 生成子节点的函数
+function generateMovieElements(data, coverSrc) {
+  // 假设你的 JSON 数据是一个包含多个电影的数组，每个电影对象都有相应的属性
+  var jsonData = data['data']['data']
+
+  // 获取目标容器元素
+  var container = document.querySelector('.movie-culture-list')
+
+  // 遍历 JSON 数据并生成对应的 <div> 元素
+  jsonData.forEach(function (item) {
+    // 创建 media 元素
+    var media = document.createElement('div')
+    media.classList.add('media')
+
+    // 创建 media-cover 元素
+    var mediaCover = document.createElement('div')
+    mediaCover.classList.add('media-cover')
+    mediaCover.style.backgroundImage =
+      'url(' + coverSrc + item.cover_image_url + ')'
+    media.appendChild(mediaCover)
+
+    // 创建 media-meta 元素
+    var mediaMeta = document.createElement('div')
+    mediaMeta.classList.add('media-meta')
+
+    // 创建 title 元素
+    var title = document.createElement('div')
+    title.classList.add('media-meta-item', 'title')
+    title.textContent = item.movie_name
+    mediaMeta.appendChild(title)
+
+    // 创建元信息元素
+    var meta = document.createElement('div')
+    meta.classList.add('media-meta-item')
+
+    // 创建作者和评分元素
+    var authorSpan = document.createElement('span')
+    authorSpan.classList.add('author')
+    authorSpan.textContent = '中国 2021'
+    meta.appendChild(authorSpan)
+
+    var starScoreSpan = document.createElement('span')
+    starScoreSpan.classList.add('star-score')
+    starScoreSpan.textContent = '★★★★★'
+    meta.appendChild(starScoreSpan)
+
+    mediaMeta.appendChild(meta)
+
+    // 创建 intro 元素
+    var intro = document.createElement('div')
+    intro.classList.add('media-meta-item', 'intro')
+    intro.textContent = item.movie_description
+    mediaMeta.appendChild(intro)
+
+    // 将生成的子元素添加到 media 元素中
+    media.appendChild(mediaMeta)
+
+    // 将 media 元素添加到目标容器中
+    container.appendChild(media)
+  })
+}
+
 // 等待文档加载完成
 $(document).ready(function () {
   // 获取背景图像元素
@@ -45,4 +107,27 @@ $(document).ready(function () {
       display: posDisplay && scrollDisplay ? 'block' : 'none',
     })
   })
+
+  // 如果存在元素.culture-list的div 元素，则执行以下代码
+  if (document.querySelector('.movie-culture-list')) {
+    // 获取元素.culture-list
+    var cultureList = document.querySelector('.movie-culture-list')
+    // 获取元素.culture-list的json-src属性值
+    var jsonSrc = cultureList.getAttribute('json-src')
+    // 获取元素.culture-list的cover-src属性值
+    var coverSrc = cultureList.getAttribute('cover-src')
+
+    // 发起异步请求获取第一页的 json 数据
+    fetch(jsonSrc)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        // 根据第一页的 json 数据生成子节点
+        generateMovieElements(data, coverSrc)
+      })
+      .catch(function (error) {
+        console.error('Error:', error)
+      })
+  }
 })
