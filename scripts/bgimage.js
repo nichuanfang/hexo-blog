fd57333bff31969628072e375ed5bef2cf857490
f18326186224 <f18326186224@gmail.com>
2023-12-12 12:44:01 +0800

    ✨ feat: 将all.min.css改为vercel托管

--- scripts/bgimage.js
+++ scripts/bgimage.js
@@ -5,6 +5,6 @@ hexo.extend.filter.register('theme_inject', function (injects) {
   )
   injects.head.raw(
     'font-awesome',
-    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">'
+    '<link rel="stylesheet" href="/css/all.min.css">'
   )
 })
