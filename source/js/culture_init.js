5a5b265f2f0d16032ad6b4382823ea3a3c186dcb
f18326186224 <f18326186224@gmail.com>
2023-12-14 18:33:31 +0800

    ✨ feat: 通过请求https://api.jaychou.site/common/init来消除SSL连接时间

--- source/js/culture_init.js
+++ source/js/culture_init.js
@@ -1,10 +1,7 @@
 // 避免打开浏览器时 Culture 页面出现空白，先请求一次数据 建立SSL连接
 // 如果SessionStorage中没有init_data，则初始化init_data
 if (!sessionStorage.getItem('init_data')) {
-  fetch('https://api.jaychou.site/trakt/movie?page=1&page_size=12')
-    .then(function (response) {
-      return response.json()
-    })
+  fetch('https://api.jaychou.site/common/init')
     .then(function (data) {
       sessionStorage.setItem('init_data', true)
     })
