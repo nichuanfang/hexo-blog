27899bebf3ea64f4f38b853d134be2710371a5a0
f18326186224 <f18326186224@gmail.com>
2024-05-30 15:29:36 +0800

    feat: 避免打开浏览器时 Culture 页面出现空白，先请求一次数据 建立SSL连接

--- source/js/culture_init.js
+++ source/js/culture_init.js
@@ -2,7 +2,7 @@
 // 如果SessionStorage中没有init_data且inited不存在，则初始化init_data
 
 // 如果当前是culture/*页面 才执行以下代码
-if (window.location.pathname == "/culture/") {
+if (window.location.pathname == "/" || window.location.pathname == "/culture/") {
     const movieDataPromise = !sessionStorage.getItem("movie_init_data") ?
         fetch("https://api.jaychou.site/trakt/movie?page=1&page_size=12").then(t => t.json()) :
         Promise.resolve(null);
